89afb9f7f4de316e0632c99cb950d38a2a3a251f
abydenes <schoolreef2@protonmail.com>
2022-10-11 19:38:23 +0300

    Refactor

--- script.js
+++ script.js
@@ -1,66 +1,90 @@
-/* eslint-disable prefer-const */
-/* eslint-disable require-jsdoc */
-/* eslint-disable max-len */
-const display = document.querySelector('#display');
 let num1 = [];
-let op = [];
 let num2 = [];
-let result = [];
+let operator = '';
+let result = 0;
+const btns = document.querySelectorAll('button');
+const display = document.querySelector('.display');
 
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('number') && op.length == 0) {
-    num1.push(e.target.textContent);
-    display.textContent += e.target.textContent;
-  } else if (e.target.classList.contains('number') && !num1.length == 0 && !op.length == 0) {
-    display.textContent = num2.join('');
-    num2.push(e.target.textContent);
-    display.textContent += e.target.textContent;
-  }
-  if (e.target.classList.contains('operator') && !num1.length == 0 && !op.length == 0 && !num2.length == 0) {
-    result = operate(+num1.join(''), op[0], +num2.join(''));
-    display.textContent = result;
-    num1.splice(0, 10, result);
-    num2 = [];
-  }
-  if (e.target.classList.contains('operator') && !num2.length == 0 && !op.length == 0) {
-    op.splice(0, 1, e.target.textContent);
-  } else if (e.target.classList.contains('operator')) {
-    op.splice(0, 1, e.target.textContent);
-  }
-  if (e.target.classList.contains('equals') && !num2.length == 0) {
-    result = operate(+num1.join(''), op[0], +num2.join(''));
-    display.textContent = roundToTwo(result);
-    num1.splice(0, 10, result);
-    num2 = [];
-    if (result == 'Infinity') display.textContent = 'hehe';
-  }
-  if (e.target.classList.contains('ac')) {
-    display.textContent = '';
-    display.innerHTML = '';
-    num1 = [];
-    op = [];
-    num2 = [];
-    result = [];
-  }
-});
-
-function roundToTwo(num) {
-  return +(Math.round(num + 'e+4') + 'e-4');
-}
-
-const operate = function(a, operator, b) {
-  switch (operator) {
+function operate(op, a, b) {
+  switch (op) {
     case '+':
       return a + b;
-      break;
     case '-':
       return a - b;
-      break;
     case '*':
       return a * b;
-      break;
     case '/':
       return a / b;
-      break;
+    default:
+      return false;
   }
-};
+}
+
+function clearDisplay() {
+  display.textContent = '0';
+  num1 = [];
+  num2 = [];
+  operator = '';
+}
+
+function populateDisplay() {
+  result = operate(operator, +num1.join(''), +num2.join(''));
+  display.textContent = parseFloat(result.toFixed(8));
+  if (!Number.isFinite(result)) {
+    display.textContent = 'hehe';
+  }
+
+  num1 = [result];
+  num2 = [];
+  operator = '';
+}
+
+function deleteFromDisplay() {
+  if (num2.length > 0) {
+    num2.pop();
+    display.textContent = num2.join('');
+  } else if (num2.length === 0) {
+    num1.pop();
+    display.textContent = num1.join('');
+  }
+}
+
+function clickNumber(e) {
+  if (display.textContent.includes('.')) {
+    document.querySelector('#dot').disabled = true;
+  } else document.querySelector('#dot').disabled = false;
+  if (operator.length === 0) {
+    num1.push(e.target.textContent);
+    display.textContent = num1.join('');
+  } else if (operator.length > 0) {
+    num2.push(e.target.textContent);
+    display.textContent = num2.join('');
+  }
+}
+
+btns.forEach((i) => {
+  i.addEventListener('click', (e) => {
+    if (e.target.classList.contains('clear')) {
+      clearDisplay();
+    }
+    if (e.target.classList.contains('delete')) {
+      deleteFromDisplay();
+    }
+    if (e.target.classList.contains('equals') && num2.length > 0) {
+      populateDisplay();
+    }
+    if (e.target.classList.contains('operator')) {
+      if (operator.length === 1) {
+        populateDisplay();
+      }
+      operator = e.target.textContent;
+    }
+    if (e.target.classList.contains('number')) {
+      clickNumber(e);
+    }
+  });
+});
+
+document.addEventListener('keydown', (e) => {
+  console.log(e);
+});
